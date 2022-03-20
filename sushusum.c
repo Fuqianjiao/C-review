ea814b685a4047be25ff71bbc48178472dcf0175
Fuqianjiao <101001960+Fuqianjiao@users.noreply.github.com>
2022-03-20 13:48:17 +0800

    统计素数求和
    C语言的习题练习

---
+++ sushusum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int count = 0;
+    int sum = 0;
+    int m, n;
+    int i, j;
+    scanf("%d %d", &m, &n);
+    //本题要求统计给定整数 M 和 N 区间内素数的个数并对它们求和。
+    for (i = m; i <= n; i++) {//在两个数字之间循环
+        for (j = 2; j <= i; j++) {//找到可以整除的数
+    if(i % j == 0) {//找到了就打破循环
+        break;
+    }
+    if(j == i) {//如果找出的数与本身相等，说明这个数是质数
+        sum += i;
+        count += 1;
+    }
+        }
+    }
+    printf("%d %d", count, sum);
+    return 0;
+}
